15b8a6c8bbb31020895b05fe8f87df675a87bcc2
Oscar Palmér <opalmer@me.com>
2025-03-18 18:52:06 +0100

    0.1.0 - hello, world

---
+++ vite.config.ts
@@ -0,0 +1,40 @@
+/// <reference types="vitest" />
+import {extname, relative} from 'node:path';
+import {fileURLToPath} from 'node:url';
+import {globSync} from 'glob';
+import {defineConfig} from 'vite';
+
+const watch = process.argv.includes('--watch');
+
+const files = globSync(watch ? './src/index.ts' : './src/**/*.ts').map(file => [
+	relative('./src', file.slice(0, file.length - extname(file).length)),
+	fileURLToPath(new URL(file, import.meta.url)),
+]);
+
+export default defineConfig({
+	base: './',
+	build: {
+		lib: {
+			entry: [],
+			formats: watch ? ['es'] : ['cjs', 'es'],
+		},
+		minify: false,
+		outDir: './dist',
+		rollupOptions: {
+			input: Object.fromEntries(files),
+			output: {
+				generatedCode: 'es2015',
+				preserveModules: true,
+			},
+		},
+	},
+	logLevel: 'silent',
+	test: {
+		coverage: {
+			include: ['src/**/*.ts'],
+			provider: 'istanbul',
+		},
+		environment: 'happy-dom',
+		watch: watch,
+	},
+});
